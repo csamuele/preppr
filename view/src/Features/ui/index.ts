0c659791ef4ca6c5d5c16e2d69847d192b813897
Samuel Coleman <samuel.e.c519@gmail.com>
2023-11-08 23:32:10 -0700

    removed DrawerOptions

--- view/src/Features/ui/index.ts
+++ view/src/Features/ui/index.ts
@@ -1,5 +1,4 @@
 export * from 'Features/ui/ErrorMessage';
-export * from 'Features/ui/DrawerOptions';
 export * from 'Features/ui/uiSlice';
 export * from 'Features/ui/Select';
 export * from 'Features/ui/Layout';
